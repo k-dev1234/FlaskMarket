aea2c1a9086b66ba6be9c37c21d2fa2ff5bd6c85
k-dev1234 <kyleyap.developer@gmail.com>
2023-12-04 00:10:35 +0800

    first commit

---
+++ market.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+@app.route("/home")
+def hello_world():
+    return render_template("home.html")
+
+
+@app.route("/about")
+def about_page():
+    return render_template("about.html")
+
+
+if __name__  == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
