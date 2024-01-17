98b2ba3ce502e2b5ef54f14f4507261480ee801e
k-dev1234 <151108934+k-dev1234@users.noreply.github.com>
2024-01-17 05:28:29 +0000

    edit start

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull kdev1234/flask-market:0.0.6.RELEASE
+docker pull kdev1234/flask-market:latest
 
 # Run the Docker image as a container
-docker run -d --name flask-app-run -p 80:80 kdev1234/flask-market:0.0.6.RELEASE
+docker run -d --name flask-app-run -p 80:80 kdev1234/flask-market:latest
